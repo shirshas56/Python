2043809b8f4254ef1e6a12635915085a7440f406
SHIRSHA SAMANTA <145319422+shirshas56@users.noreply.github.com>
2025-01-06 21:59:24 +0530

    Add files via upload

---
+++ multi_table.py
@@ -0,0 +1,4 @@
+num= int(input("Enter the number you want the multiplication atble of:"))
+print(f"The multiplication table for:{num}")
+for i in range(1,11):
+         print(f"{num} X {i}= {num*i}")
